e6d8ae7b662b0a94d28010629b74cf124765945b
Tiago Andrade <tailergamer7@gmail.com>
2021-03-09 12:29:23 -0400

    :sparkles: feat(class: Listando repositórios): Listing the repositories from github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -3,19 +3,13 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
     const [repositories, setRepositories] = useState([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
-        .then(response => response.json())
-        .then(data => setRepositories(data))
+            .then(response => response.json())
+            .then(data => setRepositories(data))
     }, []);
 
     return (
@@ -23,10 +17,9 @@ export function RepositoryList() {
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })}
             </ul>
         </section>
     )
